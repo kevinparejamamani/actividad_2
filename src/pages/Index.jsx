47235b2f762a1a1851e801f13c86880c5723a1ed
kevinparejamamani <kevinparejamamani@gmail.com>
2025-11-14 20:42:12 -0500

    [ASDSAD]

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,18 +3,13 @@ import React from 'react'
 export default function Index() {
   const html = `<header>
     <h1>Bienvenido a Proyecto Sego CON VERCEL</h1>
-    <nav>
-      <a href="products">Productos</a>
-      <a href="cart">Carrito</a>
-      <a href="login">Iniciar Sesión</a>
-      <a href="registro">Registrarse</a>
-    </nav>
+
   </header>
 
   <main class="container">
     <h2>Tu tienda en línea</h2>
     <p>Explora nuestros productos, agrega al carrito y completa tu pedido de manera fácil y rápida.</p>
-    <a href="products"><button>Ver Productos</button></a>
+   
   </main>
 
   <footer>
