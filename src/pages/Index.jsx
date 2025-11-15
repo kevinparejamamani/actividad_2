4d29afdacd364fd0c802156ed6a42d9313ba0398
kevinparejamamani <kevinparejamamani@gmail.com>
2025-11-14 19:27:47 -0500

    [ERRORES EN AL RUTA]

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 export default function Index() {
   const html = `<header>
-    <h1>Bienvenido a Proyecto Sego</h1>
+    <h1>Bienvenido a Proyecto Sego CON VERCEL</h1>
     <nav>
-      <a href="products.html">Productos</a>
-      <a href="cart.html">Carrito</a>
-      <a href="login.html">Iniciar Sesión</a>
-      <a href="registro.html">Registrarse</a>
+      <a href="products">Productos</a>
+      <a href="cart">Carrito</a>
+      <a href="login">Iniciar Sesión</a>
+      <a href="registro">Registrarse</a>
     </nav>
   </header>
 
   <main class="container">
     <h2>Tu tienda en línea</h2>
     <p>Explora nuestros productos, agrega al carrito y completa tu pedido de manera fácil y rápida.</p>
-    <a href="products.html"><button>Ver Productos</button></a>
+    <a href="products"><button>Ver Productos</button></a>
   </main>
 
   <footer>
